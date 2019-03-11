53879c1376deea4b4af170a8dc57a819d5445745
Fabien Bavent <fabien.bavent@gmail.com>
2019-03-12 00:14:15 +0100

    Initialize repository with utilities basename and cat

---
+++ sources.mk
@@ -0,0 +1,52 @@
+#      This file is part of the KoraOS project.
+#  Copyright (C) 2018  <Fabien Bavent>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+NAME=kora-utils
+VERSION=0.0-$(GIT)
+
+CFLAGS += -Wall -Wextra -Wno-unused-parameter -Wno-char-subscripts
+CFLAGS += -Wno-multichar -Wno-implicit-fallthrough
+CFLAGS += -fno-builtin -fPIC
+CFLAGS += -D_DATE_=\"'$(DATE)'\" -D_OSNAME_=\"'$(LINUX)'\"
+CFLAGS += -D_GITH_=\"'$(GIT)'\" -D_VTAG_=\"'$(VERSION)'\"
+CFLAGS += -ggdb
+
+
+COV_FLAGS += --coverage -fprofile-arcs -ftest-coverage
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# We define compilation modes and associated flags
+std_CFLAGS += $(CFLAGS)
+std_CFLAGS += -I$(topdir)/include
+
+$(eval $(call ccpl,std))
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+define utilo
+$(1)_src-y += $(srcdir)/$(1).c
+DV_UTILS += $(bindir)/$(1)
+endef
+define util
+$(eval $(call utilo,$1))
+$(eval $(call link,$1,std))
+endef
+
+$(eval $(call util,basename))
+$(eval $(call util,cat))
+
+
